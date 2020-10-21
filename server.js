06b262091a3cadac184faf0b656a209e4f907e54
thiagobergami <thiagobergamig@gmail.com>
2020-10-20 23:14:14 -0300

    Adicionando o socket.io

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 import express from 'express'
 import http from 'http'
 import createGame from './public/game.js'
+import socketio from 'socket.io'
 
 const app = express()
 const server = http.createServer(app)
+const sockets = socketio(server)
 
 app.use(express.static('public'))
 
 const game = createGame()
 
-game.addPlayer({ playerId: 'player1', playerX: 0, playerY: 0 })
-game.addPlayer({ playerId: 'player2', playerX: 2, playerY: 4 })
-game.addPlayer({ playerId: 'player3', playerX: 4, playerY: 4 })
-game.addFruit({ fruitId: 'fruit1', fruitX: 5, fruitY: 5 })
-game.addFruit({ fruitId: 'fruit2', fruitX: 3, fruitY: 3 })
-
 console.log(game.state)
+
+sockets.on('connection', (socket) => {
+    const playerId = socket.id
+    console.log(`> Player connected  on Server with id: ${playerId}`)
+
+    socket.emit('setup', game.state)
+})
 
 server.listen(3000, () => {
     console.log(`> Server listening on port: 3000`)
